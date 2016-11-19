1f7898989010253251b468196968337d23bbe7c5
Suvorov Konstantin <deedushka@gmail.com>
2016-11-19 22:23:22 +0300

    1

--- Regapharm/js/common.js
+++ Regapharm/js/common.js
@@ -3,9 +3,13 @@ $(function() {
 	$(window).scroll(function() {
 	if ($(this).scrollTop() > 1){
 	$('header').addClass("sticky");
+	$('.main_pic').addClass("mar");
+	$('.bg_vtor').addClass("mar2");
 	}
 	else{
 	$('header').removeClass("sticky");
+	$('.main_pic').removeClass("mar");
+	$('.bg_vtor').removeClass("mar2");
 	}
 	});	
 	
