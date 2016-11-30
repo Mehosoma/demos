19d3cf24a6eb49640e3e27cc4e58159107e51354
Suvorov Konstantin <deedushka@gmail.com>
2016-11-30 18:20:20 +0300

    d

--- record2/js/common.js
+++ record2/js/common.js
@@ -1,15 +1,1 @@
-$(function() {
-
-	
-
-	    if (screen.width > 991 && screen.width < 1640) {
-	        $('.child').matchHeight({
-	       	target: $('.parent')
-	    });
-	    
-	    }
-	   
-
-
-});
 
