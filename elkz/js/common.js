b7a6402799c579e718e96249a297d3223cff779c
Suvorov Konstantin <deedushka@gmail.com>
2016-12-12 17:07:13 +0300

    dd

---
+++ elkz/js/common.js
@@ -0,0 +1,20 @@
+$('.top_slider').owlCarousel({
+    stagePadding: 120,
+    loop:true,
+    margin:37,
+    center: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        },
+        1920:{
+            items:5
+        }
+    }
+})
