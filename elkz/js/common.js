21de7b06282a04ff450394d9e835b116330e0046
Suvorov Konstantin <deedushka@gmail.com>
2016-12-13 12:28:31 +0300

    222

--- elkz/js/common.js
+++ elkz/js/common.js
@@ -1,8 +1,25 @@
+$(document).ready(function () {
+   var tileSlider = $('.swiper-container');
+	tileSlider.each(function(){
+    var mySwiper = new Swiper(this, {
+        loop: true,
+        pagination: $(this).parent().find('.swiper-pagination')[0],
+        paginationType: 'fraction',
+        nextButton: $(this).parent().find('.swiper-button-next')[0],
+        prevButton: $(this).parent().find('.swiper-button-prev')[0]
+    });
+	}); 
+    
+});
+
+
+
 $('.top_slider').owlCarousel({
     stagePadding: 120,
     loop:true,
     margin:37,
     center: true,
+    autoWidth: true,
     responsive:{
         0:{
             items:1
@@ -14,7 +31,29 @@ $('.top_slider').owlCarousel({
             items:3
         },
         1920:{
-            items:5
+            items:3,                
+        }
+    }
+})
+
+$('.poster_block_wrap').owlCarousel({
+    stagePadding: 120,
+    loop:true,
+    margin:37,
+    center: true,
+    autoWidth: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        },
+        1920:{
+            items:3,                
         }
     }
 })
