6bdd3e07e66e1f9c432bc8faef049e7077419526
Suvorov Konstantin <deedushka@gmail.com>
2017-03-22 15:06:50 +0300

    123

--- Medicine/js/common.js
+++ Medicine/js/common.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
 
+	$('.popup').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		}
+		
+	});
+
+});
