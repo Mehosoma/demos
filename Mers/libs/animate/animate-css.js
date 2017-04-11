3735203847db315670f3c84806b5be283422e9e5
Suvorov Konstantin <deedushka@gmail.com>
2017-04-11 12:49:44 +0300

    fin

---
+++ Mers/libs/animate/animate-css.js
@@ -0,0 +1,28 @@
+//Animate CSS + WayPoints javaScript Plugin
+//Example: $(".element").animated("zoomInUp", "zoomOutDown");
+//Author URL: http://webdesign-master.ru
+(function($) {
+		$.fn.animated = function(inEffect, outEffect) {
+				$(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
+						if (dir === "down") {
+								$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+						} else {
+								$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+								$('.about').css("background-image", "url(./Images/background3.jpg)");
+								$('.response').css("background-image", "url(./Images/second_background.jpg)");
+						};
+				}, {
+						offset: "80%"
+				}).waypoint(function(dir) {
+						if (dir === "down") {
+								$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+								$('.about').css("background-image", "url(./Images/background3.jpg)");
+								$('.response').css("background-image", "url(./Images/second_background.jpg)");
+						} else {
+								$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+						};
+				}, {
+						offset: -$(window).height()
+				});
+		};
+})(jQuery);
