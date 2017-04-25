0c3f5ed0e0d80b670910769296b7e822e2dc15ff
Suvorov Konstantin <deedushka@gmail.com>
2017-04-25 12:03:48 +0300

    lightbox

--- bmw/js/common.js
+++ bmw/js/common.js
@@ -7,3 +7,36 @@ jQuery(function($){
    		}         
     }); 
 });
+
+$(window).load(function() {
+
+	$('.polygon-left').css({'transform':'translateX(0px) translateY(0px)', 'opacity':'1'});
+	$('.polygon-right').css({'transform':'translateX(0px) translateY(-0px)', 'opacity':'1'});
+
+});
+
+
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 200){  
+    
+    	$('.polygon-left').css({'transform':'translateX(-500px) translateY(175px)', 'opacity':'0'});
+    	$('.polygon-right').css({'transform':'translateX(500px) translateY(-250px)', 'opacity':'0'});
+ 	
+ 	}
+ 		else{
+
+ 		$('.polygon-left').css({'transform':'translateX(0px) translateY(0px)', 'opacity':'1'});
+ 		$('.polygon-right').css({'transform':'translateX(0px) translateY(-0px)', 'opacity':'1'});
+   
+  	}
+});
+
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox({
+    	
+    });
+});
+
+
