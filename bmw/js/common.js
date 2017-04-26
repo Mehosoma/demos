fbb6289b48d67c215e6645e7920c3f9c92deb384
Suvorov Konstantin <deedushka@gmail.com>
2017-04-26 14:31:12 +0300

    menu

--- bmw/js/common.js
+++ bmw/js/common.js
@@ -1,3 +1,5 @@
+
+// Кнопка заблокирована/активна
 jQuery(function($){ 
     $("input[name='disclaim']").click(function() {
         if (this.checked) {
@@ -8,10 +10,12 @@ jQuery(function($){
     }); 
 });
 
+//Анимация треугольников на главной
+
 $(window).load(function() {
 
-	$('.polygon-left').css({'transform':'translateX(0px) translateY(0px)', 'opacity':'1'});
-	$('.polygon-right').css({'transform':'translateX(0px) translateY(-0px)', 'opacity':'1'});
+	$('.polygon-left').css({'transform':'translateX(0px) translateY(0px)', 'opacity':'0.3'});
+	$('.polygon-right').css({'transform':'translateX(0px) translateY(-0px)', 'opacity':'0.3'});
 
 });
 
@@ -25,18 +29,107 @@ $(window).scroll(function() {
  	}
  		else{
 
- 		$('.polygon-left').css({'transform':'translateX(0px) translateY(0px)', 'opacity':'1'});
- 		$('.polygon-right').css({'transform':'translateX(0px) translateY(-0px)', 'opacity':'1'});
+ 		$('.polygon-left').css({'transform':'translateX(0px) translateY(0px)', 'opacity':'0.3'});
+ 		$('.polygon-right').css({'transform':'translateX(0px) translateY(-0px)', 'opacity':'0.3'});
    
   	}
 });
 
 
+//Альбом-галерея
+
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox({
-    	
+
     });
 });
 
+
+//Анимация Dropdown
+
+$('.dropdown').on('show.bs.dropdown', function(e){
+
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown(1000, function(){    
+    });
+
+});
+
+$('.dropdown').on('hide.bs.dropdown', function(e){
+
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp(500, function(){   
+  }); 
+   
+});
+
+$(window).load(function() {
+
+    $(".calendar-events-block").animated("fadeInUp", "fadeOut"); 
+
+});
+
+
+// Слайдер на мобильных разрешениях
+$(document).ready(function () {
+
+
+    //stick in the fixed 100% height behind the navbar but don't wrap it
+    $('#slide-nav.navbar-inverse').after($('<div class="inverse" id="navbar-height-col"></div>'));
+  
+    $('#slide-nav.navbar-default').after($('<div id="navbar-height-col"></div>'));  
+
+    // Enter your ids or classes
+    var toggler = '.navbar-toggle';
+    var pagewrapper = '#page-content';
+    var navigationwrapper = '.navbar-header';
+    var menuwidth = '100%'; // the menu inside the slide menu itself
+    var slidewidth = '80%';
+    var menuneg = '-100%';
+    var slideneg = '-80%';    
+
+
+    $("#slide-nav").on("click", toggler, function (e) {
+
+        var selected = $(this).hasClass('slide-active');
+
+        $('#slidemenu').stop().animate({
+            right: selected ? menuneg : '0px'
+        });
+
+        $('#navbar-height-col').stop().animate({
+            right: selected ? slideneg : '0px'
+        });
+
+        $(pagewrapper).stop().animate({
+            right: selected ? '0px' : slidewidth
+        });
+       
+
+       
+        $(this).toggleClass('slide-active', !selected);
+        $('#slidemenu').toggleClass('slide-active');
+
+
+        $('#page-content, .navbar, body').toggleClass('slide-active');
+
+
+    });
+
+
+    var selected = '#slidemenu, #page-content, body, .navbar, .navbar-header';
+
+
+    $(window).on("resize", function () {
+
+        if ($(window).width() > 767 && $('.navbar-toggle').is(':hidden')) {
+            $(selected).removeClass('slide-active');
+        }
+
+
+    });
+
+
+
+
+});
 
