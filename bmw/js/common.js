e554d500aa4f40bc2305acdf2447b82dcaacd577
Suvorov Konstantin <deedushka@gmail.com>
2017-04-27 11:46:55 +0300

    12323124145512

--- bmw/js/common.js
+++ bmw/js/common.js
@@ -65,8 +65,6 @@ $('.dropdown').on('hide.bs.dropdown', function(e){
 
 //Css animation
 
-
-
 $(window).load(function() {
 
     var windowWidth = $(window).width();
@@ -79,6 +77,12 @@ $(window).load(function() {
         $(".calendar-events-block").animated("fadeInLeft", "fadeInOut");         
     }
 
+});
+
+
+// Выравниваем блоки по высоте
+$(document).ready(function(){
+   $('.col-eq').colequalizer();
 });
 
 
@@ -139,12 +143,74 @@ $(document).ready(function () {
         if ($(window).width() > 767 && $('.navbar-toggle').is(':hidden')) {
             $(selected).removeClass('slide-active');           
         }
-
-
     });
+});
 
 
+// Slider
+$(document).ready(function(){
+    $('#lightslider').lightSlider({
+        item: 4,
+        autoWidth: false,
+        slideMove: 1, // slidemove will be 1 if loop is true
+        slideMargin: 10,
+        loop: true,
+        controls: true,
+        prevHtml: '',
+        nextHtml: '',  
+        responsive : [
+            {
+                breakpoint:2560,
+                settings: {
+                    item:4,
+                    slideMove:1,                   
+                  }
+            },
 
+            {
+                breakpoint:1500,
+                settings: {
+                    item:4,
+                    slideMove:1,
+                    
+                  }
+            },
 
+            {
+                breakpoint: 1200,
+                settings: {
+                    item:4,
+                    slideMove:1,
+                    
+                  }
+            },
+
+            {
+                breakpoint:992,
+                settings: {
+                    item:3,
+                    slideMove:1,
+                                       
+                  }
+            },
+            {
+                breakpoint:768,
+                settings: {
+                    item:2,
+                    slideMove:1,
+                                     
+                  }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:1,
+                    slideMove:1,                   
+                  
+                  }
+            }
+        ]        
+    });
+    
 });
 
