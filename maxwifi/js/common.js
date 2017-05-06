4c95f821be8ee46a86a968b51a1fd72a41d0740b
Suvorov Konstantin <deedushka@gmail.com>
2017-05-06 09:56:25 +0300

    аыпвыпры

--- maxwifi/js/common.js
+++ maxwifi/js/common.js
@@ -17,68 +17,16 @@ $(document).ready(function(){
 });
 
 // Slider
-$(document).ready(function(){
-    $('#slider').lightSlider({
-        item: 4,
-        autoWidth: false,
-        slideMove: 1, // slidemove will be 1 if loop is true
-        slideMargin: 10,
-        loop: true,
-        controls: true,
-        prevHtml: '',
-        nextHtml: '',  
-        responsive : [
-            {
-                breakpoint:2560,
-                settings: {
-                    item:4,
-                    slideMove:1,                   
-                  }
-            },
-
-            {
-                breakpoint:1500,
-                settings: {
-                    item:4,
-                    slideMove:1,
-                    
-                  }
-            },
-
-            {
-                breakpoint: 1200,
-                settings: {
-                    item:4,
-                    slideMove:1,
-                    
-                  }
-            },
-
-            {
-                breakpoint:992,
-                settings: {
-                    item:3,
-                    slideMove:1,
-                                       
-                  }
-            },
-            {
-                breakpoint:768,
-                settings: {
-                    item:2,
-                    slideMove:1,
-                                     
-                  }
-            },
-            {
-                breakpoint:480,
-                settings: {
-                    item:1,
-                    slideMove:1,                   
-                  
-                  }
-            }
-        ]        
-    });
-    
+var slider = $('#slider').lightSlider({
+    controls: false,
+    loop: true,
+    item: 4,
+    slideMove: 1,
 });
+$('#goToPrevSlide').on('click', function () {
+    slider.goToPrevSlide();
+});
+$('#goToNextSlide').on('click', function () {
+    slider.goToNextSlide();
+});
+
