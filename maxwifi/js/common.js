835cdb8bc9f6effb40f95683947dafb7f1510fa8
Suvorov Konstantin <deedushka@gmail.com>
2017-05-06 13:02:12 +0300

    admob

--- maxwifi/js/common.js
+++ maxwifi/js/common.js
@@ -22,6 +22,50 @@ var slider = $('#slider').lightSlider({
     loop: true,
     item: 4,
     slideMove: 1,
+    responsive : [
+            {
+                breakpoint:1500,
+                settings: {
+                    item:4,
+                    slideMove:1,
+                    
+                  }
+            },
+
+            {
+                breakpoint: 1200,
+                settings: {
+                    item:4,
+                    slideMove:1,
+                    
+                  }
+            },
+
+            {
+                breakpoint:992,
+                settings: {
+                    item:3,
+                    slideMove:1,
+                                       
+                  }
+            },
+            {
+                breakpoint:768,
+                settings: {
+                    item:2,
+                    slideMove:1,
+                                     
+                  }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:1,
+                    slideMove:1,                   
+                  
+                  }
+            }
+        ]        
 });
 $('#goToPrevSlide').on('click', function () {
     slider.goToPrevSlide();
