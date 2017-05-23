b41773813c0dd6c79ac0326984e53c3b72141cf7
Suvorov Konstantin <deedushka@gmail.com>
2017-05-23 20:46:08 +0300

    video

---
+++ Video/js/common.js
@@ -0,0 +1,19 @@
+var slider = $('#video-slider').lightSlider({
+        item: 1,
+        autoWidth: true,       
+        slideMove: 1, // slidemove will be 1 if loop is true
+        slideMargin: 0,
+        loop: true,
+        controls: true,
+        prevHtml: '',
+        nextHtml: ''
+});
+
+$('#goToPrevSlide').on('click', function () {
+    slider.goToPrevSlide();
+});
+$('#goToNextSlide').on('click', function () {
+    slider.goToNextSlide();
+});
+
+	
