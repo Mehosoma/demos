90f6298994c590f79429b1a248f4843b7209eb34
Suvorov Konstantin <deedushka@gmail.com>
2017-05-24 15:25:31 +0300

    aadadd

--- Video/js/common.js
+++ Video/js/common.js
@@ -19,18 +19,72 @@ $('#goToNextSlide').on('click', function () {
 
 var mySwiper = new Swiper('.swiper-container', {
     speed: 400,
-    spaceBetween: 100,
-    slidesPerView: 5,
+    spaceBetween: 100,  
+    slidesPerView: 4,
     pagination: '.swiper-pagination',
     loop: true,
     nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
+    prevButton: '.swiper-button-prev',   
 });   
 
 
 $(document).ready(function(){
     $(".popup_form").magnificPopup();
+    $('.col-eq').colequalizer(); 
+});
+
+$('a[href^="#"]').click(function(){
+//Сохраняем значение атрибута href в переменной:
+var target = $(this).attr('href');
+$('html, body').animate({scrollTop: $(target).offset().top}, 800);
+return false;
+});
+
+$('.typeit').typeIt({
+      speed: 50,
 });
 
 
+//VideoResponse
+$(document).ready(function(){
+
+    $('iframe').attr('src', $('iframe').attr('src'));
+
+});
+
+
+$(document).ready(function() {
+  $('#goToPrevSlide').on('click', function() {
+    
+    $('.play-main').each(function(){
+        var el_src = $(this).attr("src");
+        $(this).attr("src",el_src);
+      });   
+	});
+});
+
+$(document).ready(function() {
+  $('#goToNextSlide').on('click', function() {
+    
+    $('.play-main').each(function(){
+        var el_src = $(this).attr("src");
+        $(this).attr("src",el_src);
+      });
+    });
+});
+
+
+ $(document).ready(function(){
+        // Add minus icon for collapse element which is open by default
+        $(".collapse.in").each(function(){
+        	$(this).siblings(".panel-heading").css('background-image', 'url(img/arrow-up.png)');
+        });
+        
+        // Toggle plus minus icon on show hide of collapse element
+        $(".collapse").on('show.bs.collapse', function(){
+        	$(this).siblings(".panel-heading").css('background-image', 'url(img/arrow-up.png)');
+        }).on('hide.bs.collapse', function(){
+        	$(this).siblings(".panel-heading").css('background-image', 'url(img/arrow-down.png)');
+        });
+    }); 
 	
