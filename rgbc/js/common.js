794844e986ffcd56adb2e5fa7058e7be5590c7a2
Suvorov Konstantin <deedushka@gmail.com>
2017-06-03 19:08:26 +0300

    1111

---
+++ rgbc/js/common.js
@@ -0,0 +1,17 @@
+
+//Triangle Canvas
+var canvas = document.getElementById('triangle');
+var context = canvas.getContext('2d');
+context.beginPath();
+context.moveTo(0, 0);
+context.lineTo(1440, 0);
+context.lineTo(720, 100);
+context.closePath();
+context.shadowColor = 'rgba(0,0,0,0.2)';
+context.shadowBlur = 10;
+context.shadowOffsetX = 0;
+context.shadowOffsetY = 0;
+context.fillStyle = "rgb(252, 252, 252)";
+context.fill();
+
+
