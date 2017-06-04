8dc492e483435d3895528b293beb61aca5a39848
Suvorov Konstantin <deedushka@gmail.com>
2017-06-04 12:11:33 +0300

    osnova

--- rgbc/js/common.js
+++ rgbc/js/common.js
@@ -25,4 +25,67 @@ context.closePath();
 context.fillStyle = "rgb(252, 252, 252)";
 context.fill();
 
+// Выравниваем блоки по высоте
+$(document).ready(function(){
+      $('.col-eq').colequalizer();   
+});
+
+// Slider
+var slider = $('.partners-slider').lightSlider({
+    controls: false,
+    loop: true,
+    item: 4,
+    slideMove: 1,    
+    responsive : [
+            {
+                breakpoint:1500,
+                settings: {
+                    item:6,
+                    slideMove:1,
+                    
+                  }
+            },
+
+            {
+                breakpoint: 1200,
+                settings: {
+                    item:4,
+                    slideMove:1,
+                    
+                  }
+            },
+
+            {
+                breakpoint:992,
+                settings: {
+                    item:3,
+                    slideMove:1,
+                                       
+                  }
+            },
+            {
+                breakpoint:768,
+                settings: {
+                    item:2,
+                    slideMove:1,
+                                     
+                  }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:1,
+                    slideMove:1,                   
+                  
+                  }
+            }
+        ]        
+});
+$('#goToPrevSlide').on('click', function () {
+    slider.goToPrevSlide();
+});
+$('#goToNextSlide').on('click', function () {
+    slider.goToNextSlide();
+});
+
 
