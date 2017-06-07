6953d91f7d82f151367d40d71faf286b9484932f
Suvorov Konstantin <deedushka@gmail.com>
2017-06-07 10:50:36 +0300

    ввввв

--- newhouse/js/common.js
+++ newhouse/js/common.js
@@ -168,7 +168,7 @@ $(document).ready(function(){
 $('a[href^="#"]').click(function(){
 //Сохраняем значение атрибута href в переменной:
 var target = $(this).attr('href');
-$('html, body').animate({scrollTop: $(target).offset().top}, 800);
+$('html, body').animate({scrollTop: $(target).offset().top-100}, 800);
 return false;
 });
 
