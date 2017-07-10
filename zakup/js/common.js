adfadfdfc2a62e51966e6095ce7f466cb0d4e9d6
Suvorov Konstantin <deedushka@gmail.com>
2017-07-10 13:34:35 +0300

    csaf

--- zakup/js/common.js
+++ zakup/js/common.js
@@ -15,6 +15,25 @@ $(document).ready(function(){
     $(".popup_form").magnificPopup();
 });
 
+$(document).ready(function() {
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+});
+
+$('a[href^="#"]').click(function(){
+//Сохраняем значение атрибута href в переменной:
+var target = $(this).attr('href');
+$('html, body').animate({scrollTop: $(target).offset().top}, 800);
+return false;
+});
+
 
 $(document).ready(function () {
    var tileSlider = $('.swiper-container');
