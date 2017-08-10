6b28cc6054d69e145fdcbff683cea4dc30268b17
Suvorov Konstantin <deedushka@gmail.com>
2017-08-11 01:51:23 +0300

    dadadada

--- invest/js/common.js
+++ invest/js/common.js
@@ -52,3 +52,10 @@ $(document).ready(function(){
   });
 
 });
+
+$('a[href^="#"]').click(function(){
+//Сохраняем значение атрибута href в переменной:
+var target = $(this).attr('href');
+$('html, body').animate({scrollTop: $(target).offset().top}, 800);
+return false;
+});
