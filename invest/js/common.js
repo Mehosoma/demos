7f603ed9b17f08be5ab7e585289571366c095d6b
Suvorov Konstantin <deedushka@gmail.com>
2017-08-11 16:27:32 +0300

    ddasdadacc

--- invest/js/common.js
+++ invest/js/common.js
@@ -36,22 +36,6 @@ $(document).ready(function(){
 
 });
 
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    center: true,
-    loop:true,
-   
-    mouseDrag:false,
-    nav:true,
-    margin:0,
-    responsive:{
-        600:{
-            items:4
-        }
-    }
-  });
-
-});
 
 $('a[href^="#"]').click(function(){
 //Сохраняем значение атрибута href в переменной:
