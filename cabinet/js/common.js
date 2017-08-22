ad6b7a8b79eb387396a9e9fe1ba719438f62c5b9
Suvorov Konstantin <deedushka@gmail.com>
2017-08-22 15:21:23 +0300

    adadada

---
+++ cabinet/js/common.js
@@ -0,0 +1,19 @@
+$(function() {
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+});
+
+// Выравниваем блоки по высоте
+$(document).ready(function(){
+      $('.col-eq').colequalizer();   
+});
+
+
