c6159ac72a1fd1b154482001421e7b5d1afb88b2
Suvorov Konstantin <deedushka@gmail.com>
2017-08-29 16:10:39 +0300

    dda

--- cabinet/js/common.js
+++ cabinet/js/common.js
@@ -16,4 +16,20 @@ $(document).ready(function(){
       $('.col-eq').colequalizer();   
 });
 
+//Смена класса при клике для сортировки
+$(document).ready(function(){
+	$('.first-viz').on('click',function(e){
+    	e.preventDefault();
+    	$('.last-viz').removeClass('sort-active-down');
+    	$(this).addClass('sort-active');
+	});
+});
+
+$(document).ready(function(){
+	$('.last-viz').on('click',function(e){
+    	e.preventDefault();
+    	$('.first-viz').removeClass('sort-active');
+    	$(this).addClass('sort-active-down');
+	});
+});
 
