0673770250789b41b93c32a1a4e4b0c487ea0cd6
Suvorov Konstantin <deedushka@gmail.com>
2017-09-05 16:13:27 +0300

    dadd

---
+++ boss/js/common.js
@@ -0,0 +1,58 @@
+$(function() {
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+});
+
+// Выравниваем блоки по высоте
+$(document).ready(function(){
+      $('.col-eq').colequalizer();   
+});
+
+$(document).ready(function(){
+    $(".popup_form").magnificPopup();
+});
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    center:true,
+    loop:true,    
+    mouseDrag:true,
+    nav:true,
+    smartSpeed:1000,
+    autoWidth:true,   
+    margin:0,
+    responsive:{
+
+        1200:{
+            items:2
+        },        
+        900:{
+            items:3
+        },
+        480:{
+            items:3
+        },
+        320:{
+            items:3
+        }
+
+    }
+  });
+
+});
+
+
+$('a[href^="#"]').click(function(){
+//Сохраняем значение атрибута href в переменной:
+var target = $(this).attr('href');
+$('html, body').animate({scrollTop: $(target).offset().top}, 800);
+return false;
+});
