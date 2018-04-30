81b507ed4b75cb1aacdb42548da8a7090fb05585
Suvorov Konstantin <deedushka@gmail.com>
2018-04-30 08:48:16 +0300

    fsadgdsgd

--- TechDerm/js/common.js
+++ TechDerm/js/common.js
@@ -61,7 +61,33 @@ $(document).ready(function(){
   		infinite: true,
   		slidesToShow: 4,
   		slidesToScroll: 1,
-  		arrows: true
+  		arrows: true,
+  		 responsive: [
+			    {
+			      breakpoint: 1401,
+			      settings: {
+			        slidesToShow: 3,
+			        slidesToScroll: 1			        			        
+			      }
+			    },
+			    {
+			      breakpoint: 1200,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 1
+			      }
+			    },
+			    {
+			      breakpoint: 992,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1
+			      }
+			    }
+			    // You can unslick at a given breakpoint now by adding:
+			    // settings: "unslick"
+			    // instead of a settings object
+  			]
 	});
 
 });
