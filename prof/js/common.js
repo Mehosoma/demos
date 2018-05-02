d719289217797bd2e350614cf8be1342b79edbe1
Suvorov Konstantin <deedushka@gmail.com>
2018-05-02 20:55:34 +0300

    part-3

--- prof/js/common.js
+++ prof/js/common.js
@@ -31,3 +31,37 @@ $(function() {
 });
 
 
+$(document).ready(function(){
+$('.slider').slick({
+  		infinite: true,
+  		slidesToShow: 1,
+  		slidesToScroll: 1,
+  		arrows: true,
+  		 responsive: [
+			    {
+			      breakpoint: 1401,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1			        			        
+			      }
+			    },
+			    {
+			      breakpoint: 1200,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 1
+			      }
+			    },
+			    {
+			      breakpoint: 992,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1
+			      }
+			    }
+			    // You can unslick at a given breakpoint now by adding:
+			    // settings: "unslick"
+			    // instead of a settings object
+  			]
+	});
+});
