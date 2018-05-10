d2452c509e9bdf80ad9cf980c7ffe34d8b0b3ac1
Suvorov Konstantin <deedushka@gmail.com>
2018-05-10 22:17:23 +0300

    fix-11

--- lub/js/common.js
+++ lub/js/common.js
@@ -75,8 +75,10 @@ $('.slider').slick({
 });
 
 
-//Show-hide menu
+
 $(document).ready(function(){
+
+	//Show-hide menu	
     $('.click-mobile').on('click',function(e){
         e.preventDefault();        
           $('.main-menu').animate({
@@ -88,4 +90,44 @@ $(document).ready(function(){
         $('.top-menu-toggle p').toggleClass('green-color');
         $('.desktop_menu').toggleClass('w-border');               
     });
+
+    //Hover 
+    if ($(window).width() < 992) {
+	    $(".right_menu").hover(function() {
+	 		$(this).prev('.nav_link').css('background-color','transparent');
+	 		},function() {
+	 		$(this).prev('.nav_link').css('background-color','transparent');
+	 	});
+		}else{
+			$(".right_menu").hover(function() {
+	 		$(this).prev('.nav_link').css('background-color','#0086d1');
+	 		},function() {
+	 		$(this).prev('.nav_link').css('background-color','#008ddc');
+	 	});
+	}
+
+
+	//Submenu animation
+	$(".hover_drop").on('click', function (e) {
+    	e.preventDefault();
+    	$('.right_menu').addClass('show')
+    });
+
+	  $(".back").on('click', function (e) {
+    	e.preventDefault();
+      $('.right_menu').removeClass('show')
+    });
+
+});
+
+
+jQuery(function($){
+	$(document).mouseup(function (e){ // событие клика по веб-документу
+		var div = $("#select_form, #select_page "); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
+			$('.activetrigger').attr('class','trigger'); // скрываем его
+			$('.dropdownvisible').attr('class','dropdownhidden'); // скрываем его
+		}
+	});
 });
