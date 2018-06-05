a3b22565671206a27eadd65b79471b8bf2294ad8
Suvorov Konstantin <deedushka@gmail.com>
2018-06-05 19:18:55 +0300

    fix-4

--- nado/js/common.js
+++ nado/js/common.js
@@ -33,6 +33,8 @@ $(function() {
 
 
 $(document).ready(function(){
+
+	//Services-Slider
 	$('.slider').slick({
   		infinite: true,
   		slidesToShow: 1,
@@ -77,11 +79,123 @@ $(document).ready(function(){
   			]
 	});
 
+	//Clider Counter
+	var $status = $('.pagingInfo p');
+	var $current = $('.current_slide');
+    var $slickElement = $('.projects-slider');    
+    var separator = " ";
+
+    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+        //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+        var i = (currentSlide ? currentSlide : 0) + 1;        
+        $current.text(i).after(" ") + $status.text('/ ' + slick.slideCount);
+        
+    });
+
+    //Projects Slider
+	$('.projects-slider').slick({
+  		infinite: true,
+  		slidesToShow: 1,
+  		slidesToScroll: 1,  		
+  		arrows: true,
+  		fade: true,
+  		cssEase: 'linear',
+  		prevArrow: $('.slick-prev1'),
+       	nextArrow: $('.slick-next1'),
+  		 responsive: [
+			    {
+			      breakpoint: 1401,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1			        			        
+			      }
+			    },
+			    {
+			      breakpoint: 1200,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 1
+			      }
+			    },
+			    {
+			      breakpoint: 992,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1
+			      }
+			    },
+			    {
+			      breakpoint: 480,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1
+			      }
+			    },
+			    // You can unslick at a given breakpoint now by adding:
+			    // settings: "unslick"
+			    // instead of a settings object
+  			]
+	});
+
 	//InfoButton
 	$('.more_button').click(function (e) {
   		$(this).parent('.more').parent('.services-right').children('.image_wrapper').toggleClass('open-tab');
   		$(this).toggleClass('open-button')
   		$(this).parent('.more').parent('.services-right').children('.image_wrapper').children('.right_content').slideToggle();
 	})
+
+	//Next Projects Click
+	$('.next_pr p').on( "click", function() {
+		$('.projects-wrapper .slick-next1').eq( $(this).index() ).click();
+		$('iframe').attr('src', $('iframe').attr('src'));
+	});
+
+	//ScrollToSection
+	$('.block-link a[href^="."]').click(function(){
+	  //Сохраняем значение атрибута href в переменной:
+	  var target = $(this).attr('href');
+	  $('html, body').animate({scrollTop: $(target).offset().top-100}, 1000);
+		return false;
+	});
+
+	//ModalWindow
+	$(document).ready(function(){
+    	$(".popup_form").magnificPopup();
+	});
+
+	//Video
+	$('.v-click').on('click', function(){
+        $(".video-main").addClass('vis-video');
+        $(".projects .slide_arrows").addClass('vis-arr');        
+        $(this).css('display','none');
+    });
+
+    $('.close-vid').on('click', function(){
+        $(".video-main").removeClass('vis-video');
+        $(".projects .slide_arrows").removeClass('vis-arr');                
+        $('.v-click').css('display','block');
+        $('iframe').attr('src', $('iframe').attr('src'));
+    });
+
+    $('.projects-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){    	
+    	$(".video-main").removeClass('vis-video');
+        $(".projects .slide_arrows").removeClass('vis-arr');                
+        $('.v-click').css('display','block');
+        $('iframe').attr('src', $('iframe').attr('src'));
+        $('iframe').attr('src', $('iframe').attr('src'));
+    });
+
+   
+
 });
 
+//Disclaim
+jQuery(function($){ 
+    $("input[name='disclaim']").click(function() {
+        if (this.checked) {
+            $('.button_form').removeClass('disabled');
+        } else {
+            $('.button_form').addClass('disabled');
+        }         
+    }); 
+});
