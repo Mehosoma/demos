9e659e022d6d71a66cb7c346fe47e295b4c70b0e
Mehosoma <deedushka@gmail.com>
2018-11-04 13:39:38 +0300

    Signed-off-by: Mehosoma <deedushka@gmail.com>

--- header/js/common.js
+++ header/js/common.js
@@ -9,3 +9,94 @@ $(document).ready(function(){
         $("header .menu-toggle-button").toggleClass('is-active');                   
     });
 });
+
+
+$(document).ready(function(){
+  $('.top-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,      
+        arrows: true,
+         responsive: [
+            {
+              breakpoint: 1401,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1                           
+              }
+            },
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+  });
+
+   $('.goods-slider').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,      
+        arrows: true,
+         responsive: [
+            {
+              breakpoint: 1919,
+              settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1                           
+              }
+            },
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+  });
+
+});
