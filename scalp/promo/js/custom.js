d98ed4fa8532c589de113029666bcc5ff7ab2f2e
Mehosoma <deedushka@gmail.com>
2018-11-16 18:13:21 +0300

    scalp

---
+++ scalp/promo/js/custom.js
@@ -0,0 +1,35 @@
+//Owl
+$(document).ready(function(){
+  $(".owl-carousel-res").owlCarousel({
+    loop:true, 
+    smartSpeed:500,             
+    nav:false,
+    dots:false,          
+    item:2,
+    dotsEach:true,
+    responsiveClass:true,
+    margin: 50,
+    responsive:{
+        1200:{
+            items:2
+        },        
+        992:{
+            items:2
+        },
+        768:{
+            items:2
+        },
+        650:{         
+            items:2
+        },
+        480:{         
+            items:2
+        },
+        320:{
+            items:2
+        }
+
+    }
+  });
+
+});
