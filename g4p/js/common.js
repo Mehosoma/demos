91da0ce1592d789efd8a7dc53ed7e0159607bb35
Mehosoma <deedushka@gmail.com>
2018-12-14 17:44:54 +0300

    fixes

--- g4p/js/common.js
+++ g4p/js/common.js
@@ -1,5 +1,5 @@
 //ScrollToSection
-$('.main-menu a[href^="."]').click(function(){
+$('.main-menu a[href^="."], .btn-feedback').click(function(){
   //Сохраняем значение атрибута href в переменной:
   var target = $(this).attr('href');
   $('html, body').animate({scrollTop: $(target).offset().top-100}, 1000);
