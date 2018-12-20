1cae47ae5d1301037ff38936b1efe8096edd0e8a
Mehosoma <deedushka@gmail.com>
2018-12-20 11:49:47 +0300

    medeo-val

--- medeo/js/common.js
+++ medeo/js/common.js
@@ -125,5 +125,6 @@ $('.right').click(function(){
 
 $(document).ready(function() {
   $('.popup_link').magnificPopup({type:'image'});
-   $('body .leaflet-popup-content-wrapper').addClass('grub');
+
+  
 });
