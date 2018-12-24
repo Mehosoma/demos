c90b639b1dc4f5a62ebad98a68c59c9ab90177c7
Mehosoma <deedushka@gmail.com>
2018-12-24 10:45:31 +0300

    napod

---
+++ napod/js/common.js
@@ -0,0 +1,10 @@
+//Show-hide filters
+$(document).ready(function(){
+    $('.filters-block_header').on('click',function(e){
+        e.preventDefault();        
+          $(this).next('.filters-block_hidden').animate({
+              height: 'toggle'
+            }, 300, function() {
+          });        
+    });
+});
