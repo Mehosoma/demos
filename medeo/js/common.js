573ac8058a2412b23fa80364cb63d367e37bb12f
Mehosoma <deedushka@gmail.com>
2019-01-11 13:14:29 +0300

    fix-mag

--- medeo/js/common.js
+++ medeo/js/common.js
@@ -134,12 +134,8 @@ $(document).ready(function() {
       navigateByImgClick: true,
       preload: [0,1] // Will preload 0 - before current, and 1 after the current image
     },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-      titleSrc: function(item) {
-        return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-      }
-    }
+   
+   
   });
 });
 
@@ -154,11 +150,6 @@ $(document).ready(function() {
       navigateByImgClick: true,
       preload: [0,1] // Will preload 0 - before current, and 1 after the current image
     },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-      titleSrc: function(item) {
-        return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-      }
-    }
+   
   });
 });
