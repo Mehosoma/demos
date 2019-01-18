1b88429f4d65e16a88cd10d83371d95b0e552b75
Mehosoma <deedushka@gmail.com>
2019-01-18 16:11:39 +0300

    med-adapt

--- medsouz/js/common.js
+++ medsouz/js/common.js
@@ -9,33 +9,40 @@ $('.slider').slick({
       slidesToScroll: 1,         
        responsive: [
           {
+            breakpoint: 1400,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,              
+            }
+          },
+          {
             breakpoint: 1200,
             settings: {
               slidesToShow: 3,
-              slidesToScroll: 3,              
+              slidesToScroll: 1,              
             }
           },
           {
             breakpoint: 992,
             settings: {
               slidesToShow: 2,
-              slidesToScroll: 2,              
+              slidesToScroll: 1,            
             }
           },
           {
             breakpoint: 768,
             settings: {
               slidesToShow: 2,
-              slidesToScroll: 2,            
+              slidesToScroll: 1,           
             }
-          },
+          },  
           {
             breakpoint: 576,
             settings: {
               slidesToShow: 1,
               slidesToScroll: 1,           
             }
-          },
+          },          
           // You can unslick at a given breakpoint now by adding:
           // settings: "unslick"
           // instead of a settings object
@@ -50,3 +57,28 @@ $('.left').click(function(){
 $('.right').click(function(){
   $('.slider').slick('slickNext');
 })
+
+
+//Show-hide menu
+$(document).ready(function(){
+    $('.click-mobile').on('click',function(e){
+        e.preventDefault();        
+          $('.main-menu').animate({
+              height: 'toggle'
+            }, 600, function() {
+          });         
+        $("header .menu-toggle-button").toggleClass('is-active');              
+    });
+});
+
+//Show-hide Category menu
+$(document).ready(function(){
+    $('.click-cat-mobile').on('click',function(e){
+        e.preventDefault();        
+          $('.cat-menu ul').animate({
+              height: 'toggle'
+            }, 600, function() {
+          });       
+                    
+    });
+});
