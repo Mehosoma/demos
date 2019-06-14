437871271748ae7588fd7f6118da7db665533d8c
Suvorov Konstantin <deedushka@gmail.com>
2019-06-14 12:14:58 +0300

    1ss

--- promo-page/js/common.js
+++
@@ -1,7 +0,0 @@
-//ScrollToSection
-$('.nav-icons a[href^="."]').click(function(){
-  //Сохраняем значение атрибута href в переменной:
-  var target = $(this).attr('href');
-  $('html, body').animate({scrollTop: $(target).offset().top-40}, 1000);
-return false;
-});
