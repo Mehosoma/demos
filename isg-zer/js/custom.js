00582b7f306602e388bf71f35b20eb4f33289b3f
Suvorov Konstantin <deedushka@gmail.com>
2019-08-08 12:26:08 +0300

    req page

--- isg-zer/js/custom.js
+++ isg-zer/js/custom.js
@@ -1,3 +1,16 @@
+//Slider-add
+$(document).ready(function(){
 
+$('.cascade-slider').slick({
+  dots: true,  
+  speed: 500,
+  arrows: false,
+  infinite: true,
+  slidesToShow: 1, 
+   adaptiveHeight: true,
+  slidesToScroll: 1
+ 
+});
+});
 
 
