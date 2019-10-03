92198b3b961e012673d80e7a65b6ef057fd5f422
Suvorov Konstantin <deedushka@gmail.com>
2019-10-03 22:41:34 +0300

    obl-zer

---
+++ obl-zer/js/custom.js
@@ -0,0 +1,58 @@
+$(window).on('load resize', function() {
+
+   if ($(window).width() < 992) {
+    $('.bond-slider:not(.slick-initialized)').slick({
+      infinite: false,   
+      dots: true,  
+      speed: 300,
+      arrows: false, 
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+      responsive: [       
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]   
+    });
+  } else {
+    $(".bond-slider.slick-initialized").slick("unslick");
+  }
+});
+
+$(window).on('load resize', function() {
+
+   if ($(window).width() < 992) {
+    $('.bond-result-slider:not(.slick-initialized)').slick({
+      infinite: false,   
+      dots: true,  
+      speed: 300,
+      arrows: false, 
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+      responsive: [       
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]   
+    });
+  } else {
+    $(".bond-result-slider.slick-initialized").slick("unslick");
+  }
+});
+
