59c3ff4389a88c5039eb2c8f8cec716a7f468048
Suvorov Konstantin <deedushka@gmail.com>
2019-10-27 19:25:58 +0300

    preloader

--- milan/js/common.js
+++ milan/js/common.js
@@ -70,3 +70,12 @@ $('document').ready(function() {
     $('input[required]').addClass('req');
   });
 });
+
+
+ window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+  }
