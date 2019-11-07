4f6643ed485cf93b7ecca5a79cc2e18fc12c40c9
Suvorov Konstantin <deedushka@gmail.com>
2019-11-07 12:29:06 +0300

    wel

---
+++ welness/js/common.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+	$('.main-slider').slick({
+  		infinite: true,
+  		slidesToShow: 1,
+  		slidesToScroll: 1,  
+  		autoplay: false,  		
+  		arrows: false, 
+  		dots: false,  		
+	});
+	$('.responsive-slider').slick({
+  		infinite: true,
+  		slidesToShow: 1,
+  		slidesToScroll: 1,  
+  		autoplay: false,  		
+  		arrows: false, 
+  		dots: true,  		
+	});
+	$('.gallery-slider').slick({
+  		infinite: true,
+  		slidesToShow: 6,
+  		slidesToScroll: 1,  
+  		autoplay: false,  		
+  		arrows: false, 
+  		dots: false,  		
+	});
+});
+
+//Slider-Arrows
+$(function () {
+    
+    var $arrows = $('.arrows');
+    var $next = $arrows.children(".arr-right-gen");    
+    var $prev = $arrows.children(".arr-left-gen");
+    
+    var slick = $('.slider-main');
+
+    $('.arr-right-gen').on('click', function (e) {
+        var i = $next.index( this )
+        slick.eq(i).slick('slickNext');
+    });
+    $('.arr-left-gen').on('click', function (e) {
+        var i = $prev.index( this )
+        slick.eq(i).slick('slickPrev');
+    });
+
+});
+
+
+$(document).ready(function() {
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    	fixedContentPos: false,
+    	fixedBgPos: true,
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+  });
+});
+
+//ScrollToSection
+$('.main-menu a[href^="."], .buttons_btn a').click(function(){
+  //Сохраняем значение атрибута href в переменной:
+  var target = $(this).attr('href');
+  $('html, body').animate({scrollTop: $(target).offset().top-100}, 1000);
+return false;
+});
+
+$(document).ready(function() {
+  $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+      $(".site-header").addClass("scrolled");
+    } else {
+      $(".site-header").removeClass("scrolled");
+    }
+  });
+});
