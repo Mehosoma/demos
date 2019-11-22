8aeb31af155d0b208494a0c9cbccfebbbaaa735a
Suvorov Konstantin <deedushka@gmail.com>
2019-11-22 11:52:29 +0300

    quert-fr

---
+++ quest/libs/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.banner-slider').slick({
+  		infinite: true,
+  		slidesToShow: 1,
+  		slidesToScroll: 1,  
+  		autoplay: true,  		
+  		arrows: false, 
+  		dots: false,  		
+	});
+});
+
