b7c91003df2a43522ffff479634cad579dca38ee
Suvorov Konstantin <deedushka@gmail.com>
2020-01-05 21:49:49 +0300

    safasfafsa

--- football/js/common.js
+++ football/js/common.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
@@ -8,4 +10,6 @@ window.addEventListener('resize', () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 });
