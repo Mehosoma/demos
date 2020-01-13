c5e93c8903ca3d40060b302b0f45f10a2053bafa
Suvorov Konstantin <deedushka@gmail.com>
2020-01-13 21:54:10 +0300

    fsfsfsd

--- geo-new/js/common.js
+++ geo-new/js/common.js
@@ -17,3 +17,25 @@ $('.main-menu a[href^="."]').click(function(){
  $('html, body').animate({scrollTop: $(target).offset().top-100}, 1000);
 return false;
 });
+
+//Show-hide menu
+$(document).ready(function(){
+   $('.click-mobile').on('click',function(e){
+       e.preventDefault();        
+         $('.main-menu').animate({
+             height: 'toggle'
+           }, 600, function() {
+         });          
+
+       $("header .menu-toggle-button").toggleClass('is-active'); 
+   });
+
+});
+
+$(window).on('load resize', function () {
+   if ($(window).width() >= 992) {
+   		$(".main-menu").css('display','block');
+   	 } else {
+   		$(".main-menu").css('display','none');
+  	}
+});
