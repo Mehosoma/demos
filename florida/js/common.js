424e08fc6f6dda825aa2df1fd878deab7cdff112
Suvorov Konstantin <deedushka@gmail.com>
2020-01-22 18:40:37 +0300

    asfacadaa

--- florida/js/common.js
+++ florida/js/common.js
@@ -21,6 +21,29 @@ $('img.svg').each(function(){
 });
 });
 
+$('.slider-main').on('init', function(event, slick){
+    // Replace all SVG images with inline SVG  
+	$('img.svg').each(function(){
+	 var $img = $(this);
+	 var imgID = $img.attr('id');
+	 var imgClass = $img.attr('class');
+	 var imgURL = $img.attr('src');
+
+	 $.get(imgURL, function(data) {
+	   var $svg = $(data).find('svg');
+	   if (typeof imgID !== 'undefined') {
+	     $svg = $svg.attr('id', imgID);
+	   }
+	   if (typeof imgClass !== 'undefined') {
+	     $svg = $svg.attr('class', imgClass+' replaced-svg');
+	   }
+	   $svg = $svg.removeAttr('xmlns:a');
+	   $img.replaceWith($svg);
+	 });
+	});
+});
+
+
 
 //Fixed Menu
 $(document).ready(function() {
@@ -63,6 +86,41 @@ $('.responsives-slider').slick({
 		      breakpoint: 1200,
 		      settings: {
 		        slidesToShow: 3,
+		        slidesToScroll: 1,       
+		      }
+		    },
+		    {
+		      breakpoint: 992,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 1,       
+		      }
+		    },
+
+		     {
+		      breakpoint: 768,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,       
+		      }
+		    }
+	    ]   		
+	});
+});
+
+$(document).ready(function(){
+$('.blog-slider').slick({
+  		infinite: true,
+  		slidesToShow: 3,
+  		slidesToScroll: 1,  
+  		autoplay: false,  		
+  		arrows: false, 
+  		dots: false,
+  		responsive: [ 
+		    {
+		      breakpoint: 1200,
+		      settings: {
+		        slidesToShow: 2,
 		        slidesToScroll: 1,       
 		      }
 		    },
