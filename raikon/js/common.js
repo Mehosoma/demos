e4c42268f125eee40e34bd435433b1cd8070eb37
Suvorov Konstantin <deedushka@gmail.com>
2020-03-25 14:28:27 +0300

    raik-fix

--- raikon/js/common.js
+++ raikon/js/common.js
@@ -2,3 +2,31 @@ $('#file-choice').change(function() {
     if ($(this).val() != '') $(this).prev().text($(this)[0].files[0].name);
     else $(this).prev().text('Прикрепите документ');
 });
+
+//ModalWindow
+$(function () {
+	$('.popup-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+	
+   		fixedContentPos: false,
+    	fixedBgPos: true,
+   		closeOnBgClick: true,
+
+	});
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+});
+
+//Date-Time
+
+$('#datepicker').datepicker({
+   locale: 'ru-ru'
+});
+
+$('#timepicker').timepicker({
+	locale: 'ru-ru'
+});
