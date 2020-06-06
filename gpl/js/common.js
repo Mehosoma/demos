31c7d7f0dcdb094777a13bf64a90705174dd7d73
Suvorov Konstantin <deedushka@gmail.com>
2020-06-06 20:35:26 +0300

    gpl

---
+++ gpl/js/common.js
@@ -0,0 +1,104 @@
+//Svg to inline
+$(document).ready(function(){
+// Replace all SVG images with inline SVG  
+  $('img.svg').each(function(){
+   var $img = $(this);
+   var imgID = $img.attr('id');
+   var imgClass = $img.attr('class');
+   var imgURL = $img.attr('src');
+
+   $.get(imgURL, function(data) {
+     var $svg = $(data).find('svg');
+     if (typeof imgID !== 'undefined') {
+       $svg = $svg.attr('id', imgID);
+     }
+     if (typeof imgClass !== 'undefined') {
+       $svg = $svg.attr('class', imgClass+' replaced-svg');
+     }
+     $svg = $svg.removeAttr('xmlns:a');
+     $img.replaceWith($svg);
+   });
+  });
+});
+
+//ModalWindow
+$(function () {
+	$('.popup-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+	
+   		fixedContentPos: false,
+    	fixedBgPos: true,
+   		closeOnBgClick: true,
+
+	});
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+});
+
+
+
+//Show-hide menu
+$(document).ready(function(){
+   $('.click-mobile').on('click',function(e){
+       e.preventDefault();  
+         $('.main-menu-wrap').animate({
+             height: 'toggle'
+           }, 600, function() {
+         }); 
+       $("header .menu-toggle-button").toggleClass('is-active');
+ 
+   });
+
+});
+
+//Slider
+$(document).ready(function(){
+  $('.slider__main').slick({
+      infinite: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,       
+      arrows: false, 
+      dots: true,
+      draggable: true,    
+  });
+});
+
+
+//Slider-Arrows
+$(function () {    
+    var $arrows = $('.arrows');
+    var $next = $arrows.children(".arr-right");    
+    var $prev = $arrows.children(".arr-left");
+    
+    var slick = $('.slider-main');
+
+    $('.arr-right').on('click', function (e) {
+        var i = $next.index( this )
+        slick.eq(i).slick('slickNext');
+    });
+    $('.arr-left').on('click', function (e) {
+        var i = $prev.index( this )
+        slick.eq(i).slick('slickPrev');
+    });
+});
+
+//Fixed Menu
+$(document).ready(function() {
+  $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 140) {
+      $(".nav").addClass("scrolled");
+    } else {
+      $(".nav").removeClass("scrolled");
+    }
+  });
+});
+
+//Check Submenu
+$(document).ready(function() {
+ $('.nav__wrap__menu__submenu').parent('li').addClass('nav__wrap__active__submenu');
+});
