40a4bfa73d4b57cd1b099d5ef8a1a34cc716ba5b
Suvorov Konstantin <deedushka@gmail.com>
2020-08-26 09:22:01 +0300

    fix-clean

--- cleandup/js/common.js
+++ cleandup/js/common.js
@@ -46,7 +46,7 @@ $(function () {
 $(document).ready(function(){
    $('.click-mobile').on('click',function(e){
        e.preventDefault();  
-         $('.main-menu-wrap').animate({
+         $('.civil-footer__menu_wrap').animate({
              height: 'toggle'
            }, 600, function() {
          }); 
@@ -85,9 +85,19 @@ $(document).ready(function(){
       draggable: true,
       responsive: [ 
               {
-                breakpoint: 768,
+                breakpoint: 1200,
                 settings: {
-                  adaptiveHeight:true,      
+                  adaptiveHeight:true,
+                  centerMode: true      
+                }
+              },
+
+               {
+                breakpoint: 992,
+                settings: {
+                  adaptiveHeight:true,
+                  slidesToShow: 1,
+                  centerMode: true      
                 }
               },
 
@@ -107,9 +117,18 @@ $(document).ready(function(){
       draggable: true,
       responsive: [ 
               {
+                breakpoint: 1200,
+                settings: {
+                  adaptiveHeight:true,
+                  slidesToShow: 2 
+                }
+              },
+
+               {
                 breakpoint: 768,
                 settings: {
-                  adaptiveHeight:true,      
+                  adaptiveHeight:true,
+                  slidesToShow: 1,     
                 }
               },
 
@@ -129,12 +148,30 @@ $(document).ready(function(){
 
 
 //See All Link
-$(document).ready(function(){
-  $('.questions__block a').on('click',function(e){
-       e.preventDefault();  
-    $(this).closest('.questions__block').find('.questions__block_hidden').slideToggle();
-    $(this).closest('.questions__block').toggleClass('questions__block--open');
-  });
+
+$(window).on('load resize', function() {  
+  if ($(window).width() >= 767) {
+      $('.questions__block span').off('click'); 
+      $('.questions__block a').off('click');
+      $('.questions__block_list').css('display', 'block');
+      $('.questions__block_hidden').css('display', 'none');
+      $('.questions__block').removeClass('questions__block--open');
+    $('.questions__block a').on('click',function(e){
+         e.preventDefault();  
+      $(this).closest('.questions__block').find('.questions__block_hidden').slideToggle();
+      $(this).closest('.questions__block').toggleClass('questions__block--open');    
+    });
+   } else {
+      $('.questions__block span').off('click');
+      $('.questions__block_hidden').css('display', 'block');
+      $('.questions__block_list').css('display', 'none');
+      $('.questions__block').removeClass('questions__block--open');
+      $('.questions__block span').on('click',function(e){
+         e.preventDefault();  
+      $(this).closest('.questions__block').find('.questions__block_list').slideToggle();
+      $(this).closest('.questions__block').toggleClass('questions__block--open');
+    });  
+  }
 });
 
 
@@ -144,6 +181,34 @@ $(document).ready(function(){
        e.preventDefault();  
     $(this).closest('.questions__wrap').find('.questions__blocks_hidden').slideToggle();
      $(this).closest('.questions__wrap').toggleClass('questions__wrap--open');
+  });
+});
+
+
+//Btn-Close-Footer
+$(document).ready(function(){
+  $('.civil-footer__menu span').on('click',function(e){
+       e.preventDefault();  
+    $(this).closest('.civil-footer__menu').find('ul').slideToggle();
+     $(this).closest('.civil-footer__menu').toggleClass('footer__menu--open');
+  });
+});
+
+
+//Btn-Close-Footer
+$(document).ready(function(){
+  $('.civil-footer__form span').on('click',function(e){
+       e.preventDefault();  
+    $(this).closest('.civil-footer__form').find('#footer-form').slideToggle();
+     $(this).closest('.civil-footer__form').toggleClass('footer__menu--open');
+  });
+});
+
+//Btn-Filters
+$(document).ready(function(){
+  $('.store__menu_btn').on('click',function(e){
+       e.preventDefault();  
+    $(this).closest('.col-3').find('.store__menu').slideToggle();
   });
 });
 
