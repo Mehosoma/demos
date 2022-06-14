e4bf1d0a5cc386368bb9d1bded55f79967513bba
Mehosoma <mehosoma@yandex.ru>
2022-06-14 16:44:57 +0300

    academy-adapt-fixes

---
+++ academy/send.php
@@ -0,0 +1,33 @@
+<?php header('Content-Type: text/html; charset=utf-8');
+
+$fio = $_POST['name'];
+$phone = $_POST['phone'];
+$mail = $_POST['mail'];
+$mes = $_POST['message'];
+
+$fio = htmlspecialchars($fio);
+$phone = htmlspecialchars($phone);
+$mail = htmlspecialchars($mail);
+$mes = htmlspecialchars($mes);
+$fio = urldecode($fio);
+$phone = urldecode($phone);
+$mail = urldecode($mail);
+$mes = urldecode($mes);
+$fio = trim($fio);
+$phone = trim($phone);
+$mail = trim($mail);
+$mes = trim($mes);
+
+$address = "seregey-nv@mail.ru";
+$sub = "Сообщение с сайта Академия образования";
+
+$message = "Сообщение с сайта Академия образования \n
+Имя отправителя: $fio
+Телефон: $phone
+Email: $mail 
+$mes";
+
+$mail = mail($address, $sub, $message, $headers);
+ 
+echo 'ok';
+?>
