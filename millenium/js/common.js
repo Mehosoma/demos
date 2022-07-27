fb6d51445618eececa0320623c2498278d698937
Mehosoma <mehosoma@yandex.ru>
2022-07-27 13:12:16 +0300

    adapt-fix-mil
    adapt-fix-mil

--- millenium/js/common.js
+++ millenium/js/common.js
@@ -37,67 +37,77 @@ $(document).ready(function(){
 
 
 //Animation blocks on donate page
-$('a[data-toggle="tab"]').on('hide.bs.tab', function (e) {
-    var $old_tab = $($(e.target).attr("href"));
-    var $new_tab = $($(e.relatedTarget).attr("href"));
 
-    if($new_tab.index() < $old_tab.index()){
-      $old_tab.css('position', 'relative').css("right", "0").show();
-      $old_tab.animate({"right":"-100%"}, 500, function () {
-        $old_tab.css("right", 0).removeAttr("style");
-      });
-    }
-    else {
-      $old_tab.css('position', 'relative').css("left", "0").show();
-      $old_tab.animate({"left":"-100%"}, 500, function () {
-        $old_tab.css("left", 0).removeAttr("style");
-      });
-    }
-  });
+$(window).on('load resize', function () {
 
-  $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-    var $new_tab = $($(e.target).attr("href"));
-    var $old_tab = $($(e.relatedTarget).attr("href"));
+  var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
 
-    if($new_tab.index() > $old_tab.index()){
-      $new_tab.css('position', 'relative').css("right", "-2500px");
-      $new_tab.animate({"right":"0"}, 500);
-    }
-    else {
-      $new_tab.css('position', 'relative').css("left", "-2500px");
-      $new_tab.animate({"left":"0"}, 500);
-    }
-  });
+  if(!isMobile) {
 
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-    // your code on active tab shown
-  });
+  $('a[data-toggle="tab"]').on('hide.bs.tab', function (e) {
+      var $old_tab = $($(e.target).attr("href"));
+      var $new_tab = $($(e.relatedTarget).attr("href"));
 
-//News nav
-   $(document).ready(function () {
-            $("a#back").click(function(event) {
-                event.preventDefault()
-                pagenate = getGet('page');
-                if (pagenate != 0)
-                pagenate =+ Number(pagenate) - 1;
-                var url = location.pathname + "?page=" + pagenate;
-                $(location).attr('href',url);
-            });
+      if($new_tab.index() < $old_tab.index()){
+        $old_tab.css('position', 'relative').css("right", "0").show();
+        $old_tab.animate({"right":"-100%"}, 500, function () {
+          $old_tab.css("right", 0).removeAttr("style");
+        });
+      }
+      else {
+        $old_tab.css('position', 'relative').css("left", "0").show();
+        $old_tab.animate({"left":"-100%"}, 500, function () {
+          $old_tab.css("left", 0).removeAttr("style");
+        });
+      }
+    });
 
-            $("a#next").click(function(event) {
-                event.preventDefault()
-                pagenate = getGet('page');
-                pagenate =+ Number(pagenate) + 1;
-                var url = location.pathname + "?page=" + pagenate;
-                $(location).attr('href',url);
-            });
-            function getGet(name) {
-                var s = window.location.search;
-                s = s.match(new RegExp(name + '=([^&=]+)'));
-                return s ? s[1] : false;
-            }
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+      var $new_tab = $($(e.target).attr("href"));
+      var $old_tab = $($(e.relatedTarget).attr("href"));
 
-        })
+      if($new_tab.index() > $old_tab.index()){
+        $new_tab.css('position', 'relative').css("right", "-2500px");
+        $new_tab.animate({"right":"0"}, 500);
+      }
+      else {
+        $new_tab.css('position', 'relative').css("left", "-2500px");
+        $new_tab.animate({"left":"0"}, 500);
+      }
+    });
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+      // your code on active tab shown
+    });
+
+  }
+});
+
+  //News nav
+     $(document).ready(function () {
+              $("a#back").click(function(event) {
+                  event.preventDefault()
+                  pagenate = getGet('page');
+                  if (pagenate != 0)
+                  pagenate =+ Number(pagenate) - 1;
+                  var url = location.pathname + "?page=" + pagenate;
+                  $(location).attr('href',url);
+              });
+
+              $("a#next").click(function(event) {
+                  event.preventDefault()
+                  pagenate = getGet('page');
+                  pagenate =+ Number(pagenate) + 1;
+                  var url = location.pathname + "?page=" + pagenate;
+                  $(location).attr('href',url);
+              });
+              function getGet(name) {
+                  var s = window.location.search;
+                  s = s.match(new RegExp(name + '=([^&=]+)'));
+                  return s ? s[1] : false;
+              }
+
+          });
 
 
 //Fix Modal
