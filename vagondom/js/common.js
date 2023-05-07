3997c8c7ab46608dea2045386e92d9e6826cf561
Sychev Konstantin <mehosoma@yandex.ru>
2023-05-07 11:42:55 +0800

    vagon

---
+++ vagondom/js/common.js
@@ -0,0 +1,55 @@
+//Svg to inline
+$(document).ready(function(){
+// Replace all SVG images with inline SVG  
+  $('img.svg').each(function(){
+   var $img = $(this);
+   var imgID = $img.attr('id');
+   var imgClass = $img.attr('class');
+   var imgURL = $img.attr('src');
+
+   $.get(imgURL, function(data) {
+     var $svg = $(data).find('svg');
+     if (typeof imgID !== 'undefined') {
+       $svg = $svg.attr('id', imgID);
+     }
+     if (typeof imgClass !== 'undefined') {
+       $svg = $svg.attr('class', imgClass+' replaced-svg');
+     }
+     $svg = $svg.removeAttr('xmlns:a');
+     $img.replaceWith($svg);
+   });
+  });
+});
+
+
+//ScrollToSection
+$('.menu__link a[href^="."]').click(function(){
+  //Сохраняем значение атрибута href в переменной:
+  var target = $(this).attr('href');
+  $('html, body').animate({scrollTop: $(target).offset().top-130}, 1000);
+return false;
+});
+
+
+
+        $('.menu__link a').on('click touch', function() {
+        let href = $(this).attr('href');
+     
+        $('.main-menu__link')
+            .removeClass('active')
+            .filter('[href="'+ href +'"]')
+            .addClass('active');
+        });
+
+
+//Show-hide menu
+$(document).ready(function(){
+    $('.click-mobile').on('click',function(e){
+        e.preventDefault();        
+          $('.header .main-menu').animate({
+              height: 'toggle'
+            }, 600, function() {
+          });         
+        $("header .menu-toggle-button").toggleClass('is-active');             
+    });
+});
