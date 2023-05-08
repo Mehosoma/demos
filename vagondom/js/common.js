89f31a45a93962cb95475fbaa75ed08349abbf6a
Sychev Konstantin <mehosoma@yandex.ru>
2023-05-08 12:02:29 +0800

    vagon-fix

--- vagondom/js/common.js
+++ vagondom/js/common.js
@@ -53,3 +53,21 @@ $(document).ready(function(){
         $("header .menu-toggle-button").toggleClass('is-active');             
     });
 });
+
+
+//TopButton
+$(document).ready(function(){
+ 
+  $(window).scroll(function(){
+  if ($(this).scrollTop() > 100) {
+  $('.scrollup').fadeIn();
+  } else {
+  $('.scrollup').fadeOut();
+  }
+  });
+ 
+  $('.scrollup').click(function(){
+  $("html, body").animate({ scrollTop: 0 }, 600);
+  return false;
+  });
+});
