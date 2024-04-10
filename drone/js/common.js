b8a4421f70a3c9db924a39cfa543627a9845139f
Mehosoma <mehosoma@yandex.ru>
2024-04-10 08:32:09 +0800

    drone
    Drone project layout

---
+++ drone/js/common.js
@@ -0,0 +1,82 @@
+//Svg to inline
+$(document).ready(function(){
+// Replace all SVG images with inline SVG  
+  $('img.svg').each(function(){
+   var $img = $(this);
+   var imgID = $img.attr('id');
+   var imgClass = $img.attr('class');
+   var imgURL = $img.attr('src');
+
+   $.get(imgURL, function(data) {
+     var $svg = $(data).find('svg');
+     if (typeof imgID !== 'undefined') {
+       $svg = $svg.attr('id', imgID);
+     }
+     if (typeof imgClass !== 'undefined') {
+       $svg = $svg.attr('class', imgClass+' replaced-svg');
+     }
+     $svg = $svg.removeAttr('xmlns:a');
+     $img.replaceWith($svg);
+   });
+  });
+});
+
+//ScrollToSection
+$('.scrollsec a[href^="."]').click(function(){
+  //Сохраняем значение атрибута href в переменной:
+  var target = $(this).attr('href');
+  $('html, body').animate({scrollTop: $(target).offset().top-0}, 1000);
+return false;
+});
+
+//TopButton
+$(document).ready(function(){
+ 
+  $(window).scroll(function(){
+  if ($(this).scrollTop() > 100) {
+  $('.scrollup').fadeIn();
+  } else {
+  $('.scrollup').fadeOut();
+  }
+  });
+ 
+  $('.scrollup').click(function(){
+  $("html, body").animate({ scrollTop: 0 }, 600);
+  return false;
+  });
+});
+
+//Video
+$(document).ready(function() {
+  $('.popup-youtube').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+});
+
+//Gallery
+$(document).ready(function() {
+  $('.popup-gallery').magnificPopup({
+    fixedContentPos: false,
+    fixedBgPos: true,
+    delegate: '.popupim',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title');
+      }
+    }
+  });
+});
